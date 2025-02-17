45b263dec753ccd2cd99757a145c0139ea965b96
loufi84 <quentin.lataste@gmail.com>
2025-02-17 13:57:04 +0100

    Here are your numbers

---
+++ variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	for (; i <= 9; i++)
+	{
+		printf("%d", i);
+	}
+
+	printf("\n");
+
+	return (0);
+}
