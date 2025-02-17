74c61fd22d710712a047f92a6610e3ce5a5cf713
loufi84 <quentin.lataste@gmail.com>
2025-02-17 14:03:49 +0100

    Another way to see the world

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	for (; i < 10; i++)
+	{
+		putchar(i + '0');
+	}
+
+	printf("\n");
+
+	return (0);
+}
