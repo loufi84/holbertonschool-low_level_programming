6ff404fc3288ec13950b94cf24ee24c0c8689f61
loufi84 <quentin.lataste@gmail.com>
2025-02-17 10:56:20 +0100

    Now using printf

---
+++ hello_world/5-printf.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+
+/**
+ *
+ * main - Entry point
+ *
+ * Description: This program will output a simple
+ * string using printf
+ * 
+ * Return: Always 0 (Success)
+ */
+
+ int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
