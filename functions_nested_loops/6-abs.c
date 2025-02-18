24c5de6e33dc60fca2c3dd2317ece6f174dc42d2
loufi84 <quentin.lataste@gmail.com>
2025-02-18 10:36:46 +0100

    I don t like Betty

---
+++ functions_nested_loops/6-abs.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+ * _abs - calculate the absolute value of a number
+ * @n: a number
+ * Return: absolute value of a number
+ */
+
+int _abs(int n)
+{
+
+	if (n < 0)
+	{
+		return (-n);
+	}
+
+	return (n);
+}
