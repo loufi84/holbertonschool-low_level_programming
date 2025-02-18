0dd70e9e554688edf6d3cedb6ae9be4990857688
loufi84 <quentin.lataste@gmail.com>
2025-02-18 12:39:58 +0100

    Addition is a key to success

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,27 @@
+#include "main.h"
+
+/**
+ * add - add two integers together
+ * Return: an int
+ * @a: first number
+ * @b: second number
+ */
+
+int add(int a, int b)
+{
+	int result = 0;
+
+	result = a + b;
+
+	if (result >= 10)
+	{
+		_putchar('0' + (result / 10));
+		_putchar('0' + (result % 10));
+	}
+	else
+	{
+		_putchar('0' + result);
+	}
+
+	return (result);
+}
