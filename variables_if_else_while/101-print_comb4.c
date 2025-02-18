2a814932d15ff95f9e55c1af92d547897e6bc9e4
loufi84 <quentin.lataste@gmail.com>
2025-02-18 23:59:34 +0100

    The same but with an extra digit

---
+++ variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,36 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * Return: digits
+ */
+
+int main(void)
+{
+	int first_digit;
+	int second_digit;
+	int last_digit;
+
+	for (first_digit = 0; first_digit < 9; first_digit++)
+	{
+		for (second_digit = first_digit + 1; second_digit < 10; second_digit++)
+		{
+			for (last_digit = second_digit + 1; last_digit < 10; last_digit++)
+			{
+				putchar(48 + first_digit);
+				putchar(48 + second_digit);
+				putchar(48 + last_digit);
+
+				if (first_digit != 7 || second_digit != 8 || last_digit != 0)
+				{
+					putchar(',');
+					putchar(' ');
+				}
+			}
+		}
+	}
+
+	putchar('\n');
+
+	return (0);
+}
