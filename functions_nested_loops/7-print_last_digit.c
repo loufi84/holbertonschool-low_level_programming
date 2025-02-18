6ee758abb1529814c9b5e3e590e645039711bd09
loufi84 <quentin.lataste@gmail.com>
2025-02-18 10:46:40 +0100

    I want to return some things

--- functions_nested_loops/7-print_last_digit.c
+++ functions_nested_loops/7-print_last_digit.c
@@ -8,11 +8,14 @@
 
 int print_last_digit(int n)
 {
+	int last_digit;
+
 	if (n < 0)
 	{
 		n = -n;
 	}
 
-	_putchar('0' + (n % 10));
-	return (n % 10);
+	last_digit = n % 10;
+	_putchar('0' + last_digit);
+	return (last_digit);
 }
