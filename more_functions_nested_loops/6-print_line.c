a6bbeeeeba2259a767abb6b9e378318bdfb1b3f8
loufi84 <quentin.lataste@gmail.com>
2025-02-24 11:05:35 +0100

    Build the line

--- more_functions_nested_loops/6-print_line.c
+++ more_functions_nested_loops/6-print_line.c
@@ -10,13 +10,14 @@
 
 void print_line(int n)
 {
-	while (n >= 0)
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+
+	while (n > 0)
 	{
 		_putchar('_');
-		if (n <= 0)
-		{
-			_putchar('\n');
-		}
 		n--;
 	}
 	_putchar('\n');
