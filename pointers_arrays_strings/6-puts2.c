17dc600e9c9a15d778306ca5968ee9259bcec7f8
loufi84 <quentin.lataste@gmail.com>
2025-02-24 15:21:43 +0100

    Half of the string print

--- pointers_arrays_strings/6-puts2.c
+++ pointers_arrays_strings/6-puts2.c
@@ -9,8 +9,12 @@
 void puts2(char *str)
 {
 	int i;
+	int len = 0;
 
-	for (i = 0; str[i] != '\0'; i += 2)
+	while (str[len] != '\0')
+		len++;
+
+	for (i = 0; i < len; i += 2)
 	{
 		_putchar(str[i]);
 	}
