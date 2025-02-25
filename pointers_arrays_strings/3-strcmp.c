4d14c14d0aec3cc9f861a0044a831bd799240be7
loufi84 <quentin.lataste@gmail.com>
2025-02-25 16:37:34 +0100

    Those are comparisons

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -10,28 +10,14 @@
 int _strcmp(char *s1, char *s2)
 {
 	int i = 0;
-	int j = 0;
 
-	while (s1[i] != '\0')
+	while (s1[i] != '\0' && s2[i] != '\0')
 	{
-		i++;
-	}
+		if (s1[i] != s2[i])
+			return (s1[i] - s2[i]);
 
-	while (s2[j] != '\0')
-	{
-		j++;
+	i++;
 	}
 
-	if (i == j)
-	{
-		return (0);
-	}
-	else if (i > j)
-	{
-		return (15);
-	}
-	else
-	{
-		return (-15);
-	}
+	return (s1[i] - s2[i]);
 }
