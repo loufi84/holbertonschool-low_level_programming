1803f5d0bb1cac22d342ea56e4abfdd589c4d7e0
loufi84 <quentin.lataste@gmail.com>
2025-02-26 14:21:12 +0100

    This is the life of some numbers

---
+++ more_functions_nested_loops/101-print_number.c
@@ -0,0 +1,36 @@
+#include "main.h"
+
+/**
+ * print_number - print an int
+ * @n: int to print
+ * Return: void
+ */
+
+void print_number(int n)
+{
+	unsigned int num;
+
+	if (n < 0)
+	{
+		_putchar('-');
+		num = -n;
+	}
+	else
+	{
+		num = n;
+	}
+
+	if (num == 0)
+	{
+		_putchar('0');
+	}
+
+	else
+	{
+		if (num / 10 != 0)
+		{
+			print_number(num / 10);
+		}
+		_putchar(num % 10 + '0');
+	}
+}
