6149cdf0f4ea88c3a154c1e996259b96e4806e89
loufi84 <quentin.lataste@gmail.com>
2025-02-26 21:19:39 +0100

    I am a H4CK3R

--- pointers_arrays_strings/101-keygen.c
+++ pointers_arrays_strings/101-keygen.c
@@ -2,3 +2,34 @@
 #include <stdlib.h>
 #include <time.h>
 
+/* target = 2772 (Oxad4) */
+
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+
+int main(void)
+{
+	int sum = 0;
+	char password[100];
+	int index = 0;
+	int rand_char;
+
+	srand(time(0));
+
+	while (sum < 2772 - 126)
+	{
+		rand_char = (rand() % 94) + 33;
+		password[index] = rand_char;
+		sum += rand_char;
+		index++;
+	}
+
+	password[index++] = 2772 - sum;
+	password[index] = '\0';
+
+	printf("%s\n", password);
+
+	return (0);
+}
