1bf5c0c3ee8eabd630975b2f81e34cc108258e74
loufi84 <quentin.lataste@gmail.com>
2025-03-03 16:16:49 +0100

    Silence or not silence I like it the same

---
+++ argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: number of arguments
+ * @argv: value of arguments
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%d\n", argc - 1);
+
+	(void)argv;
+
+	return (0);
+}
