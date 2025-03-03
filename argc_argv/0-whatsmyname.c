63b8f334d4b8499bdcd76cd146e4e1bd328d1049
loufi84 <quentin.lataste@gmail.com>
2025-03-03 16:05:42 +0100

    Try this ?

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ * @argc: arguments count
+ * @argv: arguments values
+ * Description: print its name followed by a new line
+ * Return: Always 0 (Success)
+ */
+
+int main(int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+
+	(void)argc;
+
+	return (0);
+}
---
+++ argc_argv/README.md
@@ -0,0 +1,2 @@
+This is a new directory containing the exercises of argc and argv[]
+for Holberton School fundamentals.
