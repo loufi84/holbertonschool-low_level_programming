6f01d05ccc02e1002c67123b1efe40a21e1ce3a3
loufi84 <quentin.lataste@gmail.com>
2025-03-10 14:23:14 +0100

    Now with a new flavor of me dying on C

--- more_malloc_free/2-calloc.c
+++ more_malloc_free/2-calloc.c
@@ -11,5 +11,24 @@
 
 void *_calloc(unsigned int nmemb, unsigned int size)
 {
-	
+	unsigned int i;
+	char *ptr;
+
+	if (nmemb == 0 || size == 0)
+	{
+		return (NULL);
+	}
+
+	ptr = malloc(nmemb * size);
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; i < (nmemb * size); i++)
+	{
+		ptr[i] = 0;
+	}
+
+	return (ptr);
 }
