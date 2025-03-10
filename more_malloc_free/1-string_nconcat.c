110fa286b47cb05b7f14deff3ddc1195c9c1bfa7
loufi84 <quentin.lataste@gmail.com>
2025-03-10 14:47:52 +0100

    Trying some things

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -12,7 +12,7 @@
 
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, j, len1;
+	unsigned int i, j, len1, len2;
 	char *str;
 
 	if (s1 == NULL)
@@ -25,10 +25,12 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	}
 
 	for (len1 = 0; s1[len1] != '\0'; len1++)
-	{
-	}
-
-	str = malloc(sizeof(char) * len1 + n + 1);
+	{}
+	for (len2 = 0; s2[len2] != '\0'; len2++)
+	{}
+	if (n >= len2)
+		n = len2;
+	str = malloc(sizeof(char) * (len1 + n + 1));
 	if (str == NULL)
 	{
 		return (NULL);
@@ -38,7 +40,7 @@ char *string_nconcat(char *s1, char *s2, unsigned int n)
 	{
 		str[i] = s1[i];
 	}
-	for (j = 0; j <= n; j++)
+	for (j = 0; j < n; j++)
 	{
 		str[i + j] = s2[j];
 	}
