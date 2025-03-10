737eb0bc35cc4efd88285e57da3023f81dab715a
loufi84 <quentin.lataste@gmail.com>
2025-03-10 14:11:32 +0100

    Message quand même

---
+++ more_malloc_free/1-string_nconcat.c
@@ -0,0 +1,49 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * string_nconcat - Concatenate 2 strings
+ * @s1: First string
+ * @s2: Second string
+ * @n: number of bytes of s2 to concatenate
+ * Return: a pointer to the new string
+ */
+
+char *string_nconcat(char *s1, char *s2, unsigned int n)
+{
+	unsigned int i, j, len1;
+	char *str;
+
+	if (s1 == NULL)
+	{
+		s1 = "";
+	}
+	if (s2 == NULL)
+	{
+		s2 = "";
+	}
+
+	for (len1 = 0; s1[len1] != '\0'; len1++)
+	{
+	}
+
+	str = malloc(sizeof(char) * len1 + n + 1);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; s1[i] != '\0'; i++)
+	{
+		str[i] = s1[i];
+	}
+	for (j = 0; j <= n; j++)
+	{
+		str[i + j] = s2[j];
+	}
+
+	str[i + j] = '\0';
+
+	return (str);
+}
---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * _calloc - Allocate a memory and initialize it
+ * @nmemb: Array
+ * @size: Size of array
+ * Return: void
+ */
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	
+}
