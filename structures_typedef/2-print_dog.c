d7dc5a811c38e0d5642fb91b45aff4f2755993ca
loufi84 <quentin.lataste@gmail.com>
2025-03-10 16:22:33 +0100

    Some corrections ?

--- structures_typedef/2-print_dog.c
+++ structures_typedef/2-print_dog.c
@@ -11,26 +11,10 @@
 
 void print_dog(struct dog *d)
 {
-	if (d == NULL)
+	if (d != NULL)
 	{
-		return;
-	}
-
-	if (d->name == NULL)
-	{
-		printf("Name: (nil)\n");
-	}
-	else
-	{
-		printf("Name: %s\n", d->name);
-	}
-	printf("Age: %f\n", d->age);
-	if (d->owner == NULL)
-	{
-		printf("Owner: (nil)\n");
-	}
-	else
-	{
-		printf("Owner: %s\n", d->owner);
+		printf("Name: %s\n", (d->name) ? d->name : "(nil)");
+		printf("Age: %d\n", (d->age) ? d->age : 0);
+		printf("Owner; %s\n", (d->owner) ? d->owner : "(nil)");
 	}
 }
