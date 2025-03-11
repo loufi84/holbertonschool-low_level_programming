c4bb0f4e29efae56bcac59b6878ff5f7245afa1b
loufi84 <quentin.lataste@gmail.com>
2025-03-11 10:53:20 +0100

    Adding the first advanced project

---
+++ malloc_free/100-argstostr.c
@@ -0,0 +1,50 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * argstostr - concatenates all the args of the program
+ * @ac: Arguments count
+ * @av: Arguments values
+ * Return: Pointer to new str
+ */
+
+char *argstostr(int ac, char *av[])
+{
+	char *str;
+	int index, copi, copj;
+	int len = 0;
+	int tot_len = 0;
+
+	if (ac == 0 || av == NULL)
+	{
+		return (NULL);
+	}
+
+	for (index = 0; index < ac; index++)
+	{
+		for (; av[index][len] != '\0'; len++)
+		{}
+		tot_len += len + 1;
+	}
+
+	str = (char *)malloc(tot_len + 1);
+	if (str == NULL)
+	{
+		return (NULL);
+	}
+
+	copj = 0;
+	for (copi = 0; copi < ac; copi++)
+	{
+		for (len = 0; av[copi][len] != '\0'; len++)
+		{
+			str[copj++] = av[copi][len];
+		}
+		str[copj++] = '\n';
+	}
+
+	str[copj] = '\0';
+
+	return (str);
+}
