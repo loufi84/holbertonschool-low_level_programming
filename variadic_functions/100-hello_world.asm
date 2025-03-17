8dcf8b75a443993cca98b3bd82cab352d3abd55a
loufi84 <quentin.lataste@gmail.com>
2025-03-17 15:35:23 +0100

    I tried to do something more readable

--- variadic_functions/100-hello_world.asm
+++ variadic_functions/100-hello_world.asm
@@ -1,15 +1,21 @@
+section					.data
+												; Define the str "Hello World\n"
+	msg					db "Hello, World", 0x0A ; 0x0A is NewLine
+	len					equ $ -msg				; Calculate the length of the str
+
+
 section					.text
 global					_start
-_start:
-	mov rax, 1
-	mov rdi, 1
-	mov rsi, msg
-	mov rdx, len
-	syscall
-	mov rax, 60
-	xor rdi, rdi
-	syscall
 
-section					.data
-	msg					db "Hello, World", 10
-	len					equ $ -msg
+_start:
+	; System call: write(fd, buf, count)
+	mov rax, 1				; syscall number for write (1)
+	mov rdi, 1				; file descriptor (1 = stdout)
+	mov rsi, msg			; pointer to the str
+	mov rdx, len			; length of the str
+	syscall					; invoke the system call
+
+	; System call: exit(status)
+	mov rax, 60				; syscall number for exit (60)
+	xor rdi, rdi			; status = 0
+	syscall					; invoke the system call
