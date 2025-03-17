7dea944c076744dffb1e3b1eb6dc1e8970ebe8c1
loufi84 <quentin.lataste@gmail.com>
2025-03-17 10:09:26 +0100

    This is my try for the second task

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,29 @@
+#include "function_pointers.h"
+#include <stdio.h>
+
+/**
+ * int_index - Search for an int
+ * @array: The array to search in
+ * @size: The number of element in the array
+ * @cmp: The function to call
+ * Return: O for success, -1 if no element match
+ */
+
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i = 0;
+
+	if (size <= 0)
+		return (-1);
+
+	for (i = 0; i < size; i++)
+	{
+		cmp(array[i]);
+		if (cmp(array[i]) != 0)
+		{
+			return (i);
+		}
+	}
+
+	return (-1);
+}
