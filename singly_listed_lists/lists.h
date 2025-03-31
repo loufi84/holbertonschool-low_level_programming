fb36919257c32b9cf17b823a44c06c9beb4c8f93
Quentin <48597772+loufi84@users.noreply.github.com>
2025-03-31 11:30:23 +0200

    Updated the header file

--- singly_listed_lists/lists.h
+++ singly_listed_lists/lists.h
@@ -1,5 +1,5 @@
-#ifndef MAIN_H
-#define MAIN_H
+#ifndef LISTS_H
+#define LISTS_H
 
 #include <stddef.h>
 
@@ -25,4 +25,4 @@ list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
 
-#endif /* MAIN_H */
+#endif /* LISTS_H */
