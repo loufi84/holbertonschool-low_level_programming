c136481f3598d4296e3dce126393184091585480
Quentin <48597772+loufi84@users.noreply.github.com>
2025-04-01 18:21:39 +0200

    Correcting 3

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -27,16 +27,22 @@ void print_err(const char *msg, const char *file, int exit_code)
  * @filename: The name of the file
  * @flags: The flags wanted
  * @mode: The creation mode
+ * @exit_code: The exit code
  *
  * Return: A status code (1 for success -1 for failure)
  */
 
-int open_file(const char *filename, int flags, mode_t mode)
+int open_file(const char *filename, int flags, mode_t mode, int exit_code)
 {
 	int file = open(filename, flags, mode);
 
 	if (file == -1)
-		print_err("Can't access file", filename, (flags & O_CREAT) ? 99 : 98);
+	{
+		if (exit_code == 98)
+			print_err("Can't read from file", filename, 98);
+		else
+			print_err("Can't write to", filename, 99);
+	}
 	return (file);
 }
 
@@ -76,7 +82,7 @@ void close_file(int file)
 {
 	if (close(file) == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't close dfile %d\n", file);
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file);
 		exit(100);
 	}
 }
@@ -98,8 +104,8 @@ int main(int argc, char *argv[])
 		print_err("Usage:", "cp file_from file_to", 97);
 	umask(0002);
 
-	file_from = open_file(argv[1], O_RDONLY, 0);
-	file_to = open_file(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664);
+	file_from = open_file(argv[1], O_RDONLY, 0, 98);
+	file_to = open_file(argv[2], O_WRONLY | O_CREAT | O_TRUNC, 0664, 99);
 
 	copy_content(file_from, file_to);
 
