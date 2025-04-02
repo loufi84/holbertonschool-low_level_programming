58b3431b6240ec82454c51a22b8425ef83cfccd7
Quentin <48597772+loufi84@users.noreply.github.com>
2025-04-02 17:33:17 +0200

    Revert "Updated main.h"
    This reverts commit de6341f1ab8cc27f6d384c3f0e4faf7d6e16317a.

--- file_io/main.h
+++ file_io/main.h
@@ -8,8 +8,6 @@ int _putchar(char c);
 ssize_t read_textfile(const char *filename, size_t letters);
 int create_file(const char *filename, char *text_content);
 int append_text_to_file(const char *filename, char *text_content);
-void display_elf_header(Elf64_Ehdr *header);
-void print_error(char *message);
 
 
 #endif /* MAIN_H */
