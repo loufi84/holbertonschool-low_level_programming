7890cbf730ae2be6a19cbd81b100f4f79b2c4d35
Quentin <48597772+loufi84@users.noreply.github.com>
2025-04-02 15:08:30 +0200

    Made the 100 advanced

---
+++ singly_linked_lists/100-first.c
@@ -0,0 +1,14 @@
+#include "lists.h"
+#include <stdio.h>
+
+/**
+ * first - A function that prints a string before main
+ *
+ * Return: Nothing
+ */
+
+void first(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
